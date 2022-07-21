712c7a23fc9c7d8b81f44d58e1ada1e756a09c43
Jinhyeok <gjwlsgur123@gmail.com>
2022-07-22 08:49:00 +0900

    Test2

--- 19일차_220602/02_재귀호출03.py
+++
@@ -1,9 +0,0 @@
-### 10부터 1까지의 합계를 재귀 호출로 구현 ###
-
-def addNumber(num):
-    if num <= 1:  # num이 1 이하면 1을 반환
-        return 1
-    return num + addNumber(num-1) # 그렇지 않으면 num-1을 재귀호출하고
-                                  # num과 더한 값을 반환
-
-print(addNumber(10))
--- python_실습.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open('차량관리.csv','r', newline = '') as file:
-    csv_reader = csv.reader(file, delimiter = ',', quotechar = '"') # 읽기 객체 생성
-    # quotechar은 묶어야하는 string을 처리할 때 사용된다. 무엇으로 묶을지.
-    for line in csv_reader:
-        print(line)
